3933c236e34ef55c6f90882ad986cecc033d7c25
jch <nckdgns3167@naver.com>
2024-12-18 16:55:41 +0900

    init project (yarn berry, pnp, prettier, eslint, craco, classnames, scss)

---
+++ src/craco.config.js
@@ -0,0 +1,13 @@
+const CraroAlias = require('craco-alias')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+}
